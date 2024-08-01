8732e485de22390711bb363fa49051ec0fda0791
mango7789 <981653507@qq.com>
2024-08-01 16:15:54 +0800

    Add homework 2, finish high-order functions

---
+++ Homework/hw02/hw02.py
@@ -0,0 +1,257 @@
+from operator import add, mul
+
+square = lambda x: x * x
+
+identity = lambda x: x
+
+triple = lambda x: 3 * x
+
+increment = lambda x: x + 1
+
+
+HW_SOURCE_FILE = __file__
+
+
+def product(n, term):
+    """Return the product of the first n terms in a sequence.
+
+    n: a positive integer
+    term:  a function that takes one argument to produce the term
+
+    >>> product(3, identity)  # 1 * 2 * 3
+    6
+    >>> product(5, identity)  # 1 * 2 * 3 * 4 * 5
+    120
+    >>> product(3, square)    # 1^2 * 2^2 * 3^2
+    36
+    >>> product(5, square)    # 1^2 * 2^2 * 3^2 * 4^2 * 5^2
+    14400
+    >>> product(3, increment) # (1+1) * (2+1) * (3+1)
+    24
+    >>> product(3, triple)    # 1*3 * 2*3 * 3*3
+    162
+    """
+    "*** YOUR CODE HERE ***"
+    result = 1
+    for i in range(1, n + 1):
+        result *= term(i)
+    return result
+
+
+def accumulate(fuse, start, n, term):
+    """Return the result of fusing together the first n terms in a sequence 
+    and start.  The terms to be fused are term(1), term(2), ..., term(n). 
+    The function fuse is a two-argument commutative & associative function.
+
+    >>> accumulate(add, 0, 5, identity)  # 0 + 1 + 2 + 3 + 4 + 5
+    15
+    >>> accumulate(add, 11, 5, identity) # 11 + 1 + 2 + 3 + 4 + 5
+    26
+    >>> accumulate(add, 11, 0, identity) # 11 (fuse is never used)
+    11
+    >>> accumulate(add, 11, 3, square)   # 11 + 1^2 + 2^2 + 3^2
+    25
+    >>> accumulate(mul, 2, 3, square)    # 2 * 1^2 * 2^2 * 3^2
+    72
+    >>> # 2 + (1^2 + 1) + (2^2 + 1) + (3^2 + 1)
+    >>> accumulate(lambda x, y: x + y + 1, 2, 3, square)
+    19
+    """
+    "*** YOUR CODE HERE ***"
+    result = start
+    for i in range(1, n + 1):
+        result = fuse(result, term(i))
+    return result
+
+
+def summation_using_accumulate(n, term):
+    """Returns the sum: term(1) + ... + term(n), using accumulate.
+
+    >>> summation_using_accumulate(5, square) # square(0) + square(1) + ... + square(4) + square(5)
+    55
+    >>> summation_using_accumulate(5, triple) # triple(0) + triple(1) + ... + triple(4) + triple(5)
+    45
+    >>> # This test checks that the body of the function is just a return statement.
+    >>> import inspect, ast
+    >>> [type(x).__name__ for x in ast.parse(inspect.getsource(summation_using_accumulate)).body[0].body]
+    ['Expr', 'Return']
+    """
+    return sum([term(i) for i in range(1, n + 1)])
+
+
+def product_using_accumulate(n, term):
+    """Returns the product: term(1) * ... * term(n), using accumulate.
+
+    >>> product_using_accumulate(4, square) # square(1) * square(2) * square(3) * square()
+    576
+    >>> product_using_accumulate(6, triple) # triple(1) * triple(2) * ... * triple(5) * triple(6)
+    524880
+    >>> # This test checks that the body of the function is just a return statement.
+    >>> import inspect, ast
+    >>> [type(x).__name__ for x in ast.parse(inspect.getsource(product_using_accumulate)).body[0].body]
+    ['Expr', 'Return']
+    """
+    return product(n, term)
+
+
+def make_repeater(f, n):
+    """Returns the function that computes the nth application of f.
+
+    >>> add_three = make_repeater(increment, 3)
+    >>> add_three(5)
+    8
+    >>> make_repeater(triple, 5)(1) # 3 * (3 * (3 * (3 * (3 * 1))))
+    243
+    >>> make_repeater(square, 2)(5) # square(square(5))
+    625
+    >>> make_repeater(square, 3)(5) # square(square(square(5)))
+    390625
+    """
+    "*** YOUR CODE HERE ***"
+    def func(result):
+        for _ in range(n):
+            result = f(result)
+        return result
+    return func
+
+
+def digit_distance(n):
+    """Determines the digit distance of n.
+
+    >>> digit_distance(3)
+    0
+    >>> digit_distance(777)
+    0
+    >>> digit_distance(314)
+    5
+    >>> digit_distance(31415926535)
+    32
+    >>> digit_distance(3464660003)
+    16
+    >>> from construct_check import check
+    >>> # ban all loops
+    >>> check(HW_SOURCE_FILE, 'digit_distance',
+    ...       ['For', 'While'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    n = str(n)
+    if len(n) == 1:
+        return 0
+    difference = abs(int(n[0]) - int(n[1]))
+    return difference + digit_distance(n[1:])
+    
+
+
+def interleaved_sum(n, odd_func, even_func):
+    """Compute the sum odd_func(1) + even_func(2) + odd_func(3) + ..., up
+    to n.
+
+    >>> identity = lambda x: x
+    >>> square = lambda x: x * x
+    >>> triple = lambda x: x * 3
+    >>> interleaved_sum(5, identity, square) # 1   + 2*2 + 3   + 4*4 + 5
+    29
+    >>> interleaved_sum(5, square, identity) # 1*1 + 2   + 3*3 + 4   + 5*5
+    41
+    >>> interleaved_sum(4, triple, square)   # 1*3 + 2*2 + 3*3 + 4*4
+    32
+    >>> interleaved_sum(4, square, triple)   # 1*1 + 2*3 + 3*3 + 4*3
+    28
+    >>> from construct_check import check
+    >>> check(HW_SOURCE_FILE, 'interleaved_sum', ['While', 'For', 'Mod']) # ban loops and %
+    True
+    >>> check(HW_SOURCE_FILE, 'interleaved_sum', ['BitAnd', 'BitOr', 'BitXor']) # ban bitwise operators, don't worry about these if you don't know what they are
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    if n == 1:
+        return odd_func(1)
+    # Check whether n is odd
+    odd_flag = n // 2 * 2 != n
+    if odd_flag:
+        return odd_func(n) + interleaved_sum(n - 1, odd_func, even_func)
+    else:
+        return even_func(n) + interleaved_sum(n - 1, odd_func, even_func)
+
+
+def next_larger_coin(coin):
+    """Returns the next larger coin in order.
+    >>> next_larger_coin(1)
+    5
+    >>> next_larger_coin(5)
+    10
+    >>> next_larger_coin(10)
+    25
+    >>> next_larger_coin(2) # Other values return None
+    """
+    if coin == 1:
+        return 5
+    elif coin == 5:
+        return 10
+    elif coin == 10:
+        return 25
+
+def next_smaller_coin(coin):
+    """Returns the next smaller coin in order.
+    >>> next_smaller_coin(25)
+    10
+    >>> next_smaller_coin(10)
+    5
+    >>> next_smaller_coin(5)
+    1
+    >>> next_smaller_coin(2) # Other values return None
+    """
+    if coin == 25:
+        return 10
+    elif coin == 10:
+        return 5
+    elif coin == 5:
+        return 1
+
+def count_coins(total):
+    """Return the number of ways to make change using coins of value of 1, 5, 10, 25.
+    >>> count_coins(15)
+    6
+    >>> count_coins(10)
+    4
+    >>> count_coins(20)
+    9
+    >>> count_coins(100) # How many ways to make change for a dollar?
+    242
+    >>> count_coins(200)
+    1463
+    >>> from construct_check import check
+    >>> # ban iteration
+    >>> check(HW_SOURCE_FILE, 'count_coins', ['While', 'For'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    coins = [1, 5, 10, 25]
+    count = 0
+    
+    def count_partition(curr_coin, index):
+        nonlocal count
+        next_coin = curr_coin + coins[index]
+        if next_coin > total:
+            return
+        elif next_coin == total:
+            count += 1
+            return
+        else:
+            count_partition(next_coin, index)
+            if index < 3:
+                count_partition(next_coin, index + 1)
+            if index < 2:
+                count_partition(next_coin, index + 2)
+            if index < 1:
+                count_partition(next_coin, index + 3)
+
+    count_partition(0, 0)
+    count_partition(0, 1)
+    count_partition(0, 2)
+    count_partition(0, 3)
+    return count
+            
+        
+
